05d1503e46cf95a190617091e4016f4d5329727c
Dimitri Krassovski <dimitri@wix.com>
2014-04-22 15:27:35 +0300

    add lein-ancient and bump lib versions

--- project.clj
+++ project.clj
@@ -2,13 +2,14 @@
   :description "wix.is application"
   :url "http://wix.is/"
   :min-lein-version "2.0.0"
-  :dependencies [[org.clojure/clojure "1.5.1"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
                  [compojure "1.1.6"]
-                 [ring/ring-jetty-adapter "1.2.1"]
-                 [hiccup "1.0.4"]]
-  :plugins [[lein-ring "0.8.10"]]
+                 [ring/ring-jetty-adapter "1.2.2"]
+                 [hiccup "1.0.5"]]
+  :plugins [[lein-ring "0.8.10"]
+            [lein-ancient "0.5.5"]]
   :ring {:handler wixis.handler/app}
   :uberjar-name "wixis-standalone.jar"
   :profiles {:uberjar {:main wixis.handler, :aot :all}
   :dev {:dependencies [[javax.servlet/servlet-api "2.5"]
-                        [ring-mock "0.1.5"]]}})
+                       [ring-mock "0.1.5"]]}})
